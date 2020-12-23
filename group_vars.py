c05bd8b34e2cfe995b65818a1b733916b8a02340
Ubuntu <ubuntu@ip-172-31-68-46.ec2.internal>
2020-12-23 12:04:57 +0000

    adding more tasks

---
+++ group_vars.py
@@ -0,0 +1,52 @@
+#HostOrder group vars
+
+firstsHosts_fp = {
+    "hsrpPriority": 104,
+    "ipOffsetXfer": 2,
+    "ipOffsetLoopback": 1,
+    "ipOffsetServer": 2,
+    "deviceOrder": 1
+}
+
+secondsHosts_fp = {
+    "hsrpPriority": 103,
+    "ipOffsetXfer": 3,
+    "ipOffsetLoopback": 2,
+    "ipOffsetServer": 3,
+    "deviceOrder": 2
+}
+
+thirdsHosts_fp = {
+    "hsrpPriority": 102,
+    "ipOffsetXfer": 4,
+    "ipOffsetLoopback": 3,
+    "ipOffsetServer": 4,
+    "deviceOrder": 3    
+}
+
+fourthsHosts_fp = {
+    "hsrpPriority": 101,
+    "ipOffsetXfer": 5,
+    "ipOffsetLoopback": 4,
+    "ipOffsetServer": 5,
+    "deviceOrder": 4    
+}
+
+#Arquitecture solution group vars
+fp = {
+    "hsrpVipOffset": 1,
+    "interfaceVlanName": 'Vlan',
+    "interfaceDescriptionXferFw": 'xfer_Fw',
+    "interfaceDescriptionServer": 'serverNetwork',
+}
+#hsrpVip to be calculated per case
+# hsrpAuth to be calculated per case
+# routerId to be calculated per case
+#ospfPassword to be calculated per case
+
+hostName2OrderGroupVar = {
+    "fpDs-1": { "hostOrder": firstsHosts_fp, "design": fp },
+    "fpDs-2": { "hostOrder": secondsHosts_fp, "design": fp },
+    "fpDs-3": { "hostOrder": thirdsHosts_fp, "design": fp },
+    "fpDs-4": { "hostOrder": fourthsHosts_fp, "design": fp } 
+}
--- snippetTemplates.py
+++ snippetTemplates.py
@@ -3,6 +3,7 @@ from helper import *
 class newVrf:
 
     def __init__(self, vrfName):
+        
         self.vrfName = vrfName
 
 def task_buildConfig_newVrf_fp(playbook,role, hostName,inputPlaybook):
@@ -12,9 +13,58 @@ def task_buildConfig_newVrf_fp(playbook,role, hostName,inputPlaybook):
         "renderedSnippet": None
     }
     
-    newVrfObject = newVrf(inputPlaybook['vrfName'])
+    #setting up variables from inputPlaybook
+    vrfName = inputPlaybook['vrfName']
+    
+    newVrfObject = newVrf(vrfName)
     
     rendered_buildConfig['renderedSnippet']  = gen_snippet('snippet7', newVrfObject.__dict__)
     
     return rendered_buildConfig
     
+class newInterfaceVlanXferFw:
+
+    #dswX4
+    def __init__(self, interfaceName, interfaceNumber, interfaceDescription, vrfName, ipAddress, ipPrefixlen, ospfPassword, ospfProcess, ospfArea):
+
+        self.interfaceName = interfaceName
+        self.interfaceNumber = interfaceNumber
+        self.interfaceDescription = interfaceDescription
+        self.vrfName = vrfName        
+        self.ipAddress = ipAddress
+        self.ipPrefixlen = ipPrefixlen
+        self.ospfPassword = ospfPassword
+        self.ospfProcess = ospfProcess
+        self.ospfArea = ospfArea
+
+
+    
+def task_buildConfig_newInterfaceVlanXferFw_fp(playbook,role, hostName,inputPlaybook, groupVars):
+    
+    rendered_buildConfig = {
+        "hostName": hostName,
+        "renderedSnippet": None
+    }
+    
+    #setting up group vars
+    hostOrderGroupVar = groupVars['hostOrder']
+    designGroupVar = groupVars['design']
+    
+    #setting up variables from inputPlaybook
+    interfaceNumber = inputPlaybook['vlanId']
+    vrfName = inputPlaybook['vrfName']
+    ospfProcess = inputPlaybook['ospfProcess']
+    ospfArea = inputPlaybook['ospfArea']
+    
+    #setup variables from group vars
+    interfaceName = designGroupVar['interfaceVlanName']
+    interfaceDescription = designGroupVar['interfaceDescriptionXferFw']
+    ospfPassword = None
+    ipAddress = None
+    ipPrefixlen = None
+    
+    newInterfaceVlanXferFwObject = newInterfaceVlanXferFw(interfaceName, interfaceNumber, interfaceDescription, vrfName, ipAddress, ipPrefixlen, ospfPassword , ospfProcess, ospfArea)
+    
+    rendered_buildConfig['renderedSnippet']  = gen_snippet('snippet10', newInterfaceVlanXferFwObject.__dict__)
+    
+    return rendered_buildConfig
